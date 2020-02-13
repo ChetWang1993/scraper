0c5ab353c1c3f2e26546ff4005315f5a0e2e46f3
root <chet19931101@163.com>
2020-02-13 22:22:53 +0800

    a

--- dump_erd.py
+++ dump_erd.py
@@ -1,8 +1,9 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 import pandas as pd
 import datetime
 import sys
-sys.path.append('/Users/apple/Documents/trading/alpha/scripts/')
+root_path= '/root/'
+sys.path.append(root_path + 'scripts/')
 from utils import *
 from jqdatasdk import *
 auth('13918125129','fmttm1993')
@@ -10,13 +11,18 @@ auth('13918125129','fmttm1993')
 def date_str(d):
     return d[:4] + '-' + d[4:6] + '-' + d[6:]
 idx = '000300.XSHG'
-_erd_path = '/root/data/prod/erd/'
-erd_path = '/Users/apple/Documents/trading/alpha/data/erd/'
+erd_path = root_path + '/data/prod/erd/'
+_erd_path = '/Users/apple/Documents/trading/alpha/data/erd/'
 d = sys.argv[1]
 
 stocks = get_index_stocks(idx, date = date_str(d))
 e = get_bars(stocks, 1, unit='1d',fields=['date','open','high','low','close','volume','money'], include_now = False,
     end_dt = next_bday_from_str(d).strftime('%Y-%m-%d'), df = True)
+if 0 == e.size:
+    quit()
+print(e['date'][0])
+if e['date'][0] != datetime.strptime(d, '%Y%m%d').date():
+    quit()
 e = e.reset_index()
 e = e.drop(columns = ['level_1'])
 e = e.rename(columns = {'level_0': 'ric'})
