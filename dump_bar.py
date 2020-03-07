4170f4be901f96cda9a6b5e810661f6f4192c33b
ChetWang1993 <elwinwang1993@gmail.com>
2020-03-07 10:34:55 +0800

    add dump bar

---
+++ dump_bar.py
@@ -0,0 +1,31 @@
+#!/usr/local/bin/python3
+#/usr/bin/python3
+import pandas as pd
+import datetime
+import sys
+_root_path = '/root/'
+root_path = '/Users/apple/Documents/trading/'
+sys.path.append(root_path + 'scripts/')
+from utils import *
+bar_path = root_path + '/stock/data/bar/'
+d = sys.argv[1]
+data_path = root_path + '/stock/data/trading_days.txt'
+trading_days = pd.read_csv(data_path, sep = '\t')
+if 0 == trading_days[trading_days['date'] == date_str(d)].size:
+    print('{} is holiday'.format(d))
+    quit()
+from jqdatasdk import *
+auth('13918125129','fmttm1993')
+idx = '000300.XSHG'
+stocks = get_index_stocks(idx, date = date_str(d))
+e = get_bars(stocks, 48, unit='5m',fields=['date','open','high','low','close','volume','money'], include_now = False,
+    end_dt = next_bday_from_str(d).strftime('%Y-%m-%d'), df = True)
+if 0 == e.size:
+    quit()
+if e['date'][0].date() != datetime.strptime(d, '%Y%m%d').date():
+    quit()
+e = e.reset_index()
+e = e.drop(columns = ['level_1'])
+e = e.rename(columns = {'level_0': 'ric'})
+print(bar_path + '{}.txt'.format(d))
+e.to_csv(bar_path + '{}.txt'.format(d), sep='\t', index = False)
