decce9dc7bc283f4b6a79fd6d741c6b44b9d3e2d
ChetWang1993 <elwinwang1993@gmail.com>
2020-03-27 12:27:59 +0800

    ten

--- dump_ten.py
+++ dump_ten.py
@@ -1,25 +1,29 @@
-from jqdatasdk import *
-auth('13918125129','fmttm1993')
+#!/usr/bin/python3
+#/usr/local/bin/python3
 import pandas as pd
 import datetime
-idx = '000300.XSHG'
+root_path = '/root/'
+_root_path = '/Users/apple/Documents/trading/'
+sys.path.append(root_path + 'scripts/')
 from utils import *
-#get trading days
-dates = pd.DataFrame(data = get_trade_days(start_date = '2017-01-01', end_date = '2019-12-31'), columns = ['date'])
-#dates.to_csv('trading_days.txt', index = False, sep='\t')
-dates = list(dates['date'])
+from jqdatasdk import *
+auth('13918125129','fmttm1993')
+idx = '000300.XSHG'
+ten_path = root_path + '/data/ten/'
+d = sys.argv[1]
 
-erd = pd.DataFrame()
-for d in dates:
-    print(d)
-    stocks = get_index_stocks(idx, date = d)
-    #dump erd
-    e = get_bars(stocks, 8, unit='30m',fields=['date','open','high','low','close','volume','money'], include_now=False,end_dt=get_day_offset(d, 1), df = True)
-    e = e[e['date'].dt.time == datetime.time(10,30)]    
-    e = e.reset_index()
-    e = e.drop(columns = ['level_1'])
-    e = e.rename(columns = {'level_0': 'ric'})
-    e['date'] = e['date'].dt.date()
-    erd = erd.append(e)
-erd = erd.drop_duplicates()
-erd.to_csv('data/ten.txt', sep='\t', index = False)
+stocks = get_index_stocks(idx, date = d)
+#dump erd
+e = get_bars(stocks, 8, unit='30m',fields=['date','open','high','low','close','volume','money'], include_now=False,
+    end_dt = next_bday_from_str(d).strftime('%Y-%m-%d'), df = True)
+e = e[e['date'].dt.time == datetime.time(10,30)]
+if 0 == e.size:
+    quit()
+if e['date'][0] != datetime.strptime(d, '%Y%m%d').date():
+    quit()
+e = e.reset_index()
+e = e.drop(columns = ['level_1'])
+e = e.rename(columns = {'level_0': 'ric'})
+print(ten_path + '{}.txt'.format(d))
+
+erd.to_csv(ten_path + '{}.txt'.format(d), sep='\t', index = False)
