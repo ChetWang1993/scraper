830be44d653660892d83405e4581d401d075031f
ChetWang1993 <elwinwang1993@gmail.com>
2020-06-21 14:55:31 +0800

    dump erd

--- dump_erd.py
+++ dump_erd.py
@@ -7,13 +7,10 @@ import os
 root_path = os.path.dirname(os.path.realpath(__file__)) + '/../'
 sys.path.append(root_path + 'scripts/')
 from utils import *
-from jqdatasdk import *
-auth('13918125129','fmttm1993')
-idx = '000300.XSHG'
 erd_path = root_path + 'data/erd/'
 d = sys.argv[1]
 
-stocks = get_index_stocks(idx, date = date_str(d))
+stocks = get_universe(date_str(d))
 e = get_bars(stocks, 1, unit='1d',fields=['date','open','high','low','close','volume','money'], include_now = False,
     end_dt = next_bday_from_str(d).strftime('%Y-%m-%d'), df = True)
 if 0 == e.size:
