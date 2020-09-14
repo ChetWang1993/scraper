6c5cd8a84ff7ef2075363098d968963a27f3d65a
ChetWang1993 <elwinwang1993@gmail.com>
2020-09-14 20:11:33 +0800

    dump day

--- dump_day.py
+++ dump_day.py
@@ -1,19 +1,19 @@
-#!/usr/bin/python3
-#/usr/local/bin/python3
+#!/usr/local/bin/python3
+#/usr/bin/python3
 import pandas as pd
+import datetime
 import sys
 import os
 root_path = os.path.dirname(os.path.realpath(__file__)) + '/../'
 sys.path.append(root_path + 'scripts/')
-from jqdatasdk import *
-auth('13918125129','fmttm1993')
 from utils import *
+
 data_path = root_path + 'data/trading_days.txt'
 d = sys.argv[1]
 
 ret = get_trade_days(start_date = d, end_date = d)
-print(ret[0])
 if len(ret) == 0:
+    print('china holiday {}'.format(d))
     quit()
 trading_days = pd.read_csv(data_path, sep = '\t')
 trading_days['date'] =  pd.to_datetime(trading_days['date'], format='%Y-%m-%d').dt.date
@@ -21,4 +21,5 @@ if 0 < trading_days[trading_days['date'] == ret[0]].size:
     quit()
 trading_days = trading_days.append({'date': ret[0]}, ignore_index = True)
 trading_days = trading_days.drop_duplicates()
+trading_days = trading_days.sort_values(by=['date'])
 trading_days.to_csv(data_path, sep = '\t', index = False)
