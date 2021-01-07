98027d4d854ca33f4039af7f1de332dd1049ace3
ChetWang1993 <elwinwang1993@gmail.com>
2021-01-07 11:34:40 +0800

    dump multiple trading days

--- dump_day.py
+++ dump_day.py
@@ -1,5 +1,5 @@
-#!/usr/local/bin/python3
-#/usr/bin/python3
+#!/usr/bin/python3
+#/usr/local/bin/python3
 import pandas as pd
 import datetime
 import sys
@@ -9,17 +9,19 @@ sys.path.append(root_path + 'scripts/')
 from utils import *
 
 data_path = root_path + 'data/trading_days.txt'
-d = sys.argv[1]
+sd = sys.argv[1]
+ed = sys.argv[2]
 
-ret = get_trade_days(start_date = d, end_date = d)
+ret = get_trade_days(start_date = sd, end_date = ed)
 if len(ret) == 0:
-    print('china holiday {}'.format(d))
+    print('no bisnesss days between {} to {}'.format(sd, ed))
     quit()
 trading_days = pd.read_csv(data_path, sep = '\t')
 trading_days['date'] =  pd.to_datetime(trading_days['date'], format='%Y-%m-%d').dt.date
 if 0 < trading_days[trading_days['date'] == ret[0]].size:
     quit()
-trading_days = trading_days.append({'date': ret[0]}, ignore_index = True)
+for d in ret:
+    trading_days = trading_days.append({'date': d}, ignore_index = True)
 trading_days = trading_days.drop_duplicates()
 trading_days = trading_days.sort_values(by=['date'])
 trading_days.to_csv(data_path, sep = '\t', index = False)
